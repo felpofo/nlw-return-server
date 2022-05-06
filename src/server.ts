35d8eebbb2108ce8462bd5720d166570461d1486
felpofo <felipepitolpuhl@gmail.com>
2022-05-06 16:42:48 -0300

    added port env variable

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3030, () => {
-  console.log("Server is running on port 3030");
+app.listen(process.env.PORT || 3030, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3030}`);
 });
